94c8d2e98c48908e473b660a0b544745f28b1be9
nishanthan <nishanthan@codevus.com>
2022-06-02 10:26:20 +0530

    final deployment prep

--- project-af-server/server.js
+++ project-af-server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../project-af-client/build", "index.html")
+      path.join(__dirname, "..", "project-af-client", "build", "index.html")
     );
   });
 } else {
