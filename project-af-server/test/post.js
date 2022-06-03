e1faa91084355716da7cfba30ccc7bf8b5e4dded
User <IT20224134@my.sliit.lk>
2022-06-03 19:27:56 +0530

    LAST COMMIT

--- project-af-server/test/post.js
+++ project-af-server/test/post.js
@@ -1,17 +1,36 @@
 const chai = require("chai");
 const chaihttp = require("chai-http");
 const server = require("../server");
-const route = require("../routes/topic");
+const routetopic = require("../routes/topic");
+const routesupervisor = require("../routes/supervisor");
 
 chai.should();
 chai.use(chaihttp);
 
-describe("GET Api", () => {
+describe("GET Api OF TOPIC REGISTRATION ", () => {
   //Test Get
   describe("GET All TOPICS", () => {
     it("It should return all list topics", (done) => {
       chai
-        .request(route)
+        .request(routetopic)
+        .get("/")
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("object");
+          response.body.should.have.property("status");
+          response.body.should.have.property("data");
+
+          done();
+        });
+    });
+  });
+});
+describe("GET Api OF SUPERVISOR ", () => {
+  //Test Get
+  describe("GET All SUPERVISOR", () => {
+    it("It should return all list supervisors", (done) => {
+      chai
+        .request(routesupervisor)
         .get("/")
         .end((err, response) => {
           response.should.have.status(200);
